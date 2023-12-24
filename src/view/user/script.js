3794b633dc0e97e55d4cc973b216b4789cba46ca
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-12-23 18:25:21 -0600

    Se borraron los archivos

--- src/view/user/script.js
+++
@@ -1,6 +0,0 @@
-const password = document.getElementById("password");
-const show = document.getElementById("show");
-
-show.onchange = function (e) {
-  password.type = show.checked ? "text" : "password";
-};
